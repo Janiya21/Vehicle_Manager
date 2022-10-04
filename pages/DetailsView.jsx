f0f9ccb2d7892a99b7c0671d8755e9bb71e3b506
JanithSandaru <janithsandaru999@gmail.com>
2022-10-04 13:10:04 +0530

    added

--- pages/DetailsView.jsx
+++ pages/DetailsView.jsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React, { useState } from 'react'
-import { Stack, Box, Input, Center, Button, Heading, Pressable, ScrollView } from "native-base";
-import { EvilIcons } from '@expo/vector-icons';
+import { Stack, Box, Input, Center, Button, NativeBaseProvider, Heading, Pressable, ScrollView } from "native-base";
+import { Ionicons } from '@expo/vector-icons';
 
 export default function AddVehicle() {
 
@@ -13,86 +13,92 @@ export default function AddVehicle() {
     const [mileage, setmileage] = new useState('');
     const [show, setShow] = React.useState(false);
 
+
     return (
-        <ScrollView flex={"1"} style={
+        <ScrollView style={
             {
-                backgroundColor: "#004466",
+                backgroundColor: "#004466"
             }
         } >
-            <Box flex={"1"} top="20" >
+
+            <Box flex={"1"} top="10" >
                 <Center flex={"3"} w="100%" >
                     <Box flex={"2"} w="100%" h="100%" >
                         <Center>
-                            <EvilIcons name="user" size={120} color="white" />
+                            <Ionicons name="ios-car-sport-sharp" size={120} color="white" />
                         </Center>;
                     </Box>
                 </Center>
-                <Center top="2">
-                    <Heading color="white">LLLL</Heading>
-                </Center>
             </Box>
 
+            <ScrollView flex={"1"} style={
+                {
+                    backgroundColor: "#005580",
+                    minHeight: "94vh",
+                    width: "90vw",
+                    margin: "2.4vh"
+                }
+            }>
 
-            <Box flex={"1"} top="1/2">
-                <Stack w="90%" marginLeft="5">
-                    <Box alignItems="center">
-                        <Input placeholder="Username" w="80%" />
-                    </Box>
-                    <Box mt="5" alignItems="center">
-                        <Input placeholder="Password" w="80%" />
-                    </Box>
-                    <Box mt="3" alignItems="center">
-                        <Button mt="5" w="40%" success>
-                            <Text>Log In</Text>
-                        </Button>
-                    </Box>
-                </Stack>
-            </Box>
-            <Box flex={"1"} top="1/2">
-                <Stack w="90%" marginLeft="5">
-                    <Box alignItems="center">
-                        <Input placeholder="Username" w="80%" />
-                    </Box>
-                    <Box mt="5" alignItems="center">
-                        <Input placeholder="Password" w="80%" />
-                    </Box>
-                    <Box mt="3" alignItems="center">
-                        <Button mt="5" w="40%" success>
-                            <Text>Log In</Text>
-                        </Button>
-                    </Box>
-                </Stack>
-            </Box>
-            <Box flex={"1"} top="1/2">
-                <Stack w="90%" marginLeft="5">
-                    <Box alignItems="center">
-                        <Input placeholder="Username" w="80%" />
-                    </Box>
-                    <Box mt="5" alignItems="center">
-                        <Input placeholder="Password" w="80%" />
-                    </Box>
-                    <Box mt="3" alignItems="center">
-                        <Button mt="5" w="40%" success>
-                            <Text>Log In</Text>
-                        </Button>
-                    </Box>
-                </Stack>
-            </Box>
-            <Box flex={"1"} top="1/2">
-                <Stack w="90%" marginLeft="5">
-                    <Box alignItems="center">
-                        <Input placeholder="Username" w="80%" />
-                    </Box>
-                    <Box mt="5" alignItems="center">
-                        <Input placeholder="Password" w="80%" />
-                    </Box>
-                    <Box mt="3" alignItems="center">
-                        <Button mt="5" w="40%" success>
-                            <Text>Log In</Text>
-                        </Button>
-                    </Box>
-                </Stack>
-            </Box>
+                <Box flex={"1"} top="10">
+                    <Center flex={"3"} w="100%" >
+                        <Box flex={"2"} w="100%" h="100%" >
+                            <Center>
+                                <Box style={{backgroundColor:"#004466"}} p="12" rounded="xl" _text={{
+                                    fontSize: 'md',
+                                    fontWeight: 'medium',
+                                    color: 'warmGray.50',
+                                    textAlign: 'center'
+                                }}>
+                                    This is a Box with Linear Gradient
+                                </Box>;
+                                <Box style={{backgroundColor:"#004466"}} p="12" rounded="xl" _text={{
+                                    fontSize: 'md',
+                                    fontWeight: 'medium',
+                                    color: 'warmGray.50',
+                                    textAlign: 'center'
+                                }}>
+                                    This is a Box with Linear Gradient
+                                </Box>;
+
+                                <Box style={{backgroundColor:"#004466"}} p="12" rounded="xl" _text={{
+                                    fontSize: 'md',
+                                    fontWeight: 'medium',
+                                    color: 'warmGray.50',
+                                    textAlign: 'center'
+                                }}>
+                                    This is a Box with Linear Gradient
+                                </Box>;
+                                <Box style={{backgroundColor:"#004466"}} p="12" rounded="xl" _text={{
+                                    fontSize: 'md',
+                                    fontWeight: 'medium',
+                                    color: 'warmGray.50',
+                                    textAlign: 'center'
+                                }}>
+                                    This is a Box with Linear Gradient
+                                </Box>;
+                                <Box style={{backgroundColor:"#004466"}} p="12" rounded="xl" _text={{
+                                    fontSize: 'md',
+                                    fontWeight: 'medium',
+                                    color: 'warmGray.50',
+                                    textAlign: 'center'
+                                }}>
+                                    This is a Box with Linear Gradient
+                                </Box>;
+                                <Box style={{backgroundColor:"#004466"}} p="12" rounded="xl" _text={{
+                                    fontSize: 'md',
+                                    fontWeight: 'medium',
+                                    color: 'warmGray.50',
+                                    textAlign: 'center'
+                                }}>
+                                    This is a Box with Linear Gradient
+                                </Box>;
+                            </Center>;
+                        </Box>
+                    </Center>
+                </Box>
+
+            </ScrollView>
         </ScrollView>
     )
 }
